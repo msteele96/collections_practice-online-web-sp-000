f7b1174786a9cc692ac8c7b9b05afb5a5ea2144d
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 16:33:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,3 +36,12 @@ end
 def sum_array(array)
   array.reduce(:+)
 end
+
+def add_s(array)
+  array.each_with_index.collect { |element, index| 
+    if index == 1
+      
+    else  element<<"s"
+    end
+   }  
+end
