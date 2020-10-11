2fd8e24cd81df9b8a128e47257f47a4a84e34a0e
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 15:56:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,7 +29,7 @@ def kesha_maker(array)
   kesha_names = []
   array.each do |kesha|
     binding.pry
-    kesha_names.push(kesha[2]= $)
+    kesha_names.push(kesha[2]= "$")
   end
   return kesha_names
 end
